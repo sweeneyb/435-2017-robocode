90c8f7ded8de32eee018a11d6a6ea4a4616e6ead
sweeneyb <tab126@gmail.com>
2017-03-25 14:37:37 -0400

    change before round 2 semi in raleigh; add more time right auto

--- org/usfirst/frc/team435/robot/Automodes/RightFieldAuto.java
+++ org/usfirst/frc/team435/robot/Automodes/RightFieldAuto.java
@@ -1,10 +1,8 @@
 package org.usfirst.frc.team435.robot.Automodes;
 import org.usfirst.frc.team435.robot.commands.DriveForward;
-import org.usfirst.frc.team435.robot.commands.DriveToTarget;
 import org.usfirst.frc.team435.robot.commands.GyroTurn;
 import org.usfirst.frc.team435.robot.commands.OperateGearMechanism;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 public class RightFieldAuto extends CommandGroup {
@@ -12,13 +10,15 @@ public class RightFieldAuto extends CommandGroup {
 		
 		double right_gyro_turn = Preferences.getInstance().getDouble("right_gyro_turn", 30);
 		double right_drive_time = Preferences.getInstance().getDouble("right_drive_time", 1.5);
+		double right_drive_time_after_turn = Preferences.getInstance().getDouble("right_drive_time_after_turn", 0.5);
 		
 		addSequential(new OperateGearMechanism(false, 0.4));
 		addSequential(new DriveForward(.5, right_drive_time));
-		DriverStation.reportWarning("gyro turn" + right_gyro_turn, false);
 		addSequential(new GyroTurn(right_gyro_turn, true));
-		addSequential(new DriveToTarget());
+		// addSequential(new DriveToTarget());
 		// addSequential(new YawCorrection());
-		addSequential(new DriveForward(.5, .1));
+
+		addSequential(new DriveForward(.5, right_drive_time_after_turn));
+		// addSequential(new DriveForward(-.5, .2));
 	}
 }
